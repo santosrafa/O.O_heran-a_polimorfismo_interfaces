8dd247243d19fcbb84fc9c6f11c40d713b15a69c
Your Name <ti.rafaelteles@gmail.com>
2022-09-17 15:04:46 -0300

    autoload.php

--- banco.php
+++ banco.php
@@ -1,19 +1,13 @@
 <?php
 
-namespace Alura\Banco\Modelo\Conta\Titular;
-
-require_once 'Modelo/Conta/conta.php';
-require_once 'Modelo/Endereco.php';
-require_once 'Modelo/Pessoa.php';
-require_once 'Modelo/Conta/Titular.php';
-require_once 'Modelo/cpf.php';
+require 'autoload.php';
 
 use Alura\Banco\Modelo\Conta\titular;
 use Alura\Banco\Modelo\Endereco;
 use Alura\Banco\Modelo\Cpf;
 use Alura\Banco\Modelo\Conta\Conta;
 
-$endereco = new Endereco('Goiania', 'Vera Cruz', 'Minha Rua', '10C');
+$endereco = new Endereco('São Paulo', 'Vera Cruz', 'Minha Rua', '10C');
 $rafael = new titular(new Cpf('123.456.789-10'), 'Rafael Teles', $endereco);
 $primeiraConta = new Conta($rafael);
 $primeiraConta->depositar(500);
@@ -24,3 +18,4 @@ echo $primeiraConta->recuperarSaldo() . PHP_EOL;          //Chamando o metodo re
 echo $primeiraConta->recuperarCpfTitular() . PHP_EOL;
 
 echo Conta::recuperaNumeroDeContas();
+
